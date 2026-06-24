fc94377cd84b89faa834bef394ec0ce929e79267
Théo Mesnil <mesniltheo@gmail.com>
2026-06-24 08:29:36 +0200

    fix: crash on projects json

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ const projects: ProjectProps[] = [
     name: 'FlyTogether',
   },
   {
-    appleStore 'whattowatch-votre-guide-ultime/id6761733801',
+    appleStore: 'whattowatch-votre-guide-ultime/id6761733801',
     description: 'WhatToWatch is the simplest and fastest way to discover movies and series',
     googlePlay: 'com.theomesnil.WhatToWatch',
     image: whatToWatchImage,
